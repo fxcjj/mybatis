40ecd533bf173a3f8ece55a0860af8741977e2e5
罗利华 <luolihua@beadwallet.com>
2020-04-08 19:04:01 +0800

    mybatis02 for test

--- mybatis01/src/main/java/com/vic/mapper/UserMapper.java
+++ mybatis01/src/main/java/com/vic/mapper/UserMapper.java
@@ -2,9 +2,6 @@ package com.vic.mapper;
 
 import com.vic.entity.User;
 
-import java.util.List;
-import java.util.Map;
-
 
 /**
  * 用户dao
@@ -24,37 +21,5 @@ public interface UserMapper {
 	 * @param user
 	 */
 	Integer insert(User user);
-	
-	/**
-	 * 更新用户
-	 * @param user
-	 */
-	Integer update(User user);
-	
-	/**
-	 * 选择更新
-	 * @param user
-	 */
-	Integer updateBySelective(User user);
-	
-	/**
-	 * 逻辑删除用户
-	 * @param id
-	 * @return
-	 */
-	Integer delete(Long id);
-	
-	/**
-	 * 根据ids查询用户列表
-	 * @return
-	 */
-	List<User> queryByIds(List<Long> idList);
-	
-	/**
-	 * 模糊分页查询
-	 * @param param
-	 * @return
-	 */
-	List<User> fuzzyByCondition(Map<String, Object> param);
-	
+
 }
--- mybatis02/src/test/java/com/vic/UserTest.java
+++ mybatis02/src/test/java/com/vic/UserTest.java
@@ -4,6 +4,7 @@ package com.vic;
 import com.vic.entity.User;
 import com.vic.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -16,7 +17,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Date;
+import java.util.*;
+import java.util.Map.Entry;
 
 
 /**
@@ -63,15 +65,59 @@ public class UserTest {
 		
 		Assert.assertNotNull("没找到数据", user);
 	}
-
+	
+	@Test
+	public void testQueryByIds() {
+		List<Long> idList = new ArrayList<Long>();
+		idList.add(1L);
+		idList.add(2L);
+		idList.add(3L);
+		List<User> userList = userMapper.queryByIds(idList);
+		System.out.println(userList);
+	}
+	
+	@Test
+	public void testFuzzyByCondition() {
+		Map<String, Object> param = new HashMap<String, Object>();
+//		param.put("hello", "上海");
+		param.put("startIndex", 0);
+		param.put("pageSize", 10);
+		List<User> fuzzyByCondition = userMapper.fuzzyByCondition(param);
+		System.out.println(fuzzyByCondition);
+	}
+	
 	@Test
 	public void testInsert() {
 		User user = new User();
-		user.setAddress("add");
-		user.setAge(12);
+		user.setAddress("add6");
+		user.setAge(6);
 		user.setBirthday(new Date());
-		user.setName("name1");
+		user.setName("name6");
 		userMapper.insert(user);
 	}
-
+	
+	@Test
+	public void testUpdate() {
+		User user = new User();
+//		user.setId(3L);
+		user.setAddress("上海静安区1");
+		user.setAge(0);
+		user.setBirthday(new Date());
+		user.setName("marti11n");
+		user.setDeleteFlag(1);
+		userMapper.update(user);
+	}
+	
+	
+	@Test
+	public void testAliases() {
+		Configuration conf = sqlSessionFactory.getConfiguration();
+		Map<String, Class<?>> typeAliases = conf.getTypeAliasRegistry().getTypeAliases();
+		for(Entry<String, Class<?>> entry : typeAliases.entrySet()) {
+		    System.out.println(entry.getKey() + " ================> " + entry.getValue().getSimpleName());
+		}
+		
+	}
+	
+	
 }
