d8b78493f0e6bf470db4eae878fb3982c5bbe013
罗利华 <luolihua@beadwallet.com>
2020-05-19 17:40:34 +0800

    algebra

---
+++ mybatis06/src/main/java/com/vic/mapper/StudentMapper.java
@@ -0,0 +1,118 @@
+package com.vic.mapper;
+
+import com.vic.entity.Student;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * @author Victor
+ */
+public interface StudentMapper {
+	
+	/**
+	 * 根据用户id查询
+	 * @param id
+	 * @return
+	 */
+	Student queryUserById(Long id);
+
+	/**
+	 * 新增
+	 * @param student
+	 */
+	int insert(Student student);
+
+	/**
+	 * 查询正常学生、姓名模糊匹配
+	 * @param s
+	 * @return
+	 */
+	List<Student> findNormalStudentWithNameLike(Student s);
+
+	/**
+	 * 测试choose,when,otherwise
+	 * @param s
+	 * @return
+	 */
+	List<Student> testChoose(Student s);
+
+	/**
+	 * 测试where
+	 * @param s
+	 * @return
+	 */
+	List<Student> testWhere(Student s);
+
+	/**
+	 * 使用trim代替where
+	 * @param s
+	 * @return
+	 */
+	List<Student> testTrimInsteadOfWhere(Student s);
+
+	/**
+	 * 测试set
+	 * @param s
+	 * @return
+	 */
+	int testSet(Student s);
+
+	/**
+	 * 使用trim代替set
+	 * @param s
+	 * @return
+	 */
+	int testTrimInsteadOfSet(Student s);
+
+	/**
+	 * 更新用户
+	 * @param student
+	 */
+	Integer updateAnyway(Student student);
+
+
+	/**
+	 * foreach
+	 * @param ids
+	 * @return
+	 */
+	List<Student> findStudentIn1(List<Long> ids);
+
+	/**
+	 * foreach
+	 * @return
+	 */
+	List<Student> findStudentIn2(List<Long> ids);
+
+	/**
+	 * 逻辑删除用户
+	 * @param id
+	 * @return
+	 */
+	Integer delete(Long id);
+
+	/**
+	 * 使用注解更新
+	 * @param s
+	 * @return
+	 */
+	@Update({"</script>",
+		"update Student",
+		"<set>",
+		"	<if test='name != null'>name = #{name},</if>",
+		"	<if test='age != null'>age = #{age},</if>",
+		"	<if test='birthday != null'>birthday = #{birthday},</if>",
+		"	<if test='address != null'>address = #{address}</if>",
+		"</set>",
+		"where id = #{id}",
+	"</script>"})
+	int testUpdateWithScriptElement(Student s);
+
+	/**
+	 * 测试bind元素
+	 * @param s
+	 * @return
+	 */
+	List<Student> testBindElement(Student s);
+}
---
+++ mybatis06/src/test/java/com/vic/StudentTest.java
@@ -0,0 +1,214 @@
+package com.vic;
+
+
+import com.vic.entity.Student;
+import com.vic.mapper.StudentMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * user模块测试
+ * @author Victor
+ */
+public class StudentTest {
+	
+	SqlSessionFactory sqlSessionFactory = null;
+	SqlSession sqlSession = null;
+	StudentMapper studentMapper = null;
+
+	// 使用slf4j-api中的接口
+	Logger log = LoggerFactory.getLogger(StudentTest.class);
+	
+	@Before
+	public void getSessionFactory() {
+		String conf = "mybatis-config.xml";
+		Reader reader;
+		try {
+			reader = Resources.getResourceAsReader(conf);
+			sqlSessionFactory  = new SqlSessionFactoryBuilder().build(reader);
+			sqlSession = sqlSessionFactory.openSession();
+			studentMapper = sqlSession.getMapper(StudentMapper.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 关闭session
+	 */
+	@After
+	public void closeSessionFactory() {
+		sqlSession.close();
+	}
+
+
+	/**
+	 * 插入用户
+	 */
+	@Test
+	public void testInsert() {
+		Student student = new Student();
+		student.setAddress("ad2");
+		student.setAge(2);
+		student.setBirthday(new Date());
+		student.setName("vic2");
+		studentMapper.insert(student);
+	}
+
+	@Test
+	public void testQueryById() {
+		Student student = studentMapper.queryUserById(1L);
+		System.out.println(student);
+	}
+
+	/**
+	 * foreach
+	 */
+	@Test
+	public void testForeach1() {
+		List<Long> idList = new ArrayList<Long>();
+		idList.add(1L);
+		idList.add(2L);
+		idList.add(3L);
+		List<Student> list = studentMapper.findStudentIn1(idList);
+		System.out.println(list);
+	}
+
+	/**
+	 * foreach
+	 */
+	@Test
+	public void testForeach2() {
+		List<Long> idList = new ArrayList<Long>();
+		idList.add(1L);
+		idList.add(2L);
+		idList.add(3L);
+		List<Student> list = studentMapper.findStudentIn2(idList);
+		System.out.println(list);
+	}
+
+	/**
+	 * if
+	 */
+	@Test
+	public void testFindNormalStudentWithNameLike() {
+		Student s = new Student();
+		s.setName("name");
+		List<Student> list = studentMapper.findNormalStudentWithNameLike(s);
+		System.out.println(list);
+	}
+
+	/**
+	 * choose、when、otherwise
+	 */
+	@Test
+	public void testChoose() {
+		Student s = new Student();
+//		s.setName("name");
+//		s.setAddress("add");
+		List<Student> list = studentMapper.testChoose(s);
+		System.out.println(list);
+	}
+
+	/**
+	 * trim、where、set
+	 */
+	@Test
+	public void testWhere() {
+		Student s = new Student();
+		s.setName("vic");
+//		s.setAddress("add");
+		List<Student> list1 = studentMapper.testWhere(s);
+		System.out.println("where语句结果：");
+		System.out.println(list1);
+
+		List<Student> list2 = studentMapper.testTrimInsteadOfWhere(s);
+		System.out.println("使用trim代替where语句结果：");
+		System.out.println(list2);
+	}
+
+	/**
+	 * trim、where、set
+	 */
+	@Test
+	public void testSet() {
+		Student s = new Student();
+		s.setId(1L);
+		s.setName("martin");
+		s.setAge(18);
+		s.setAddress("shanghai");
+		int row1 = studentMapper.testSet(s);
+		System.out.println("set语句结果：");
+		System.out.println(row1);
+	}
+
+	/**
+	 * trim、where、set
+	 */
+	@Test
+	public void testTrimInsteadOfSet() {
+		Student s = new Student();
+		s.setId(2L);
+		s.setName("dina");
+		s.setAge(20);
+		s.setAddress("american");
+		int row1 = studentMapper.testTrimInsteadOfSet(s);
+		System.out.println("使用trim代替set语句结果：");
+		System.out.println(row1);
+	}
+
+	/**
+	 * 强制更新
+	 */
+	@Test
+	public void testUpdate() {
+		Student student = new Student();
+//		student.setId(3L);
+		student.setAddress("上海静安区1");
+		student.setAge(0);
+		student.setBirthday(new Date());
+		student.setName("marti11n");
+		student.setDeleteFlag(1);
+		studentMapper.updateAnyway(student);
+	}
+
+	/**
+	 * 使用@Update注解中的script元素
+	 * 未测试通过，语法错误！
+	 */
+	@Test
+	public void testUpdateWithScriptElement() {
+		Student s = new Student();
+		s.setId(3L);
+		s.setName("jack");
+		s.setAge(21);
+		s.setAddress("shangdong");
+		int row = studentMapper.testUpdateWithScriptElement(s);
+		System.out.println("使用@Update注解中的script元素：" + row);
+	}
+
+	/**
+	 * 测试bind元素
+	 */
+	@Test
+	public void testBindElement() {
+		Student s = new Student();
+		s.setName("vic");
+		List<Student> list = studentMapper.testBindElement(s);
+		System.out.println(list);
+	}
+}
